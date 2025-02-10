bbfcb5e44f6a467f4028435bf4c7eef7f1296a91
DHARM <opdsbanasya@gmail.com>
2025-02-10 23:00:33 +0530

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <header className='w-full h-[10vh] flex items-center justify-between gap-2 shadow-lg px-10'>
+        <header className='w-full h-[10vh] flex items-center justify-between gap-2 shadow-lg px-5 pr-10'>
             <div className='w-3/12 flex gap-4'>
                 <FiMenu className='text-xl' />
                 <img alt="YouTube" src={ytLogo} className='w-28 object-cover'/>
@@ -16,9 +16,9 @@ const Header = () => {
                 <input type='text' placeholder='Search' className='w-8/12 px-4 py-2  border rounded-l-full outline-none ' />
                 <button className='px-4 py-[11px] text-lg border rounded-r-full outline-none bg-gray-50'><IoIosSearch /></button>
             </div>
-            <div className='w-1/12 flex items-end gap-4 text-xl' >
-                <PiDotsThreeVertical  className='' />
-                <FaUserCircle className='' />
+            <div className='w-1/12 flex justify-end gap-4 text-xl' >
+                <PiDotsThreeVertical />
+                <FaUserCircle />
             </div>
         </header>
     )
