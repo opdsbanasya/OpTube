5ab6c0ff26ce258f7160771f8cedaa8a95241c4b
DHARM <opdsbanasya@gmail.com>
2025-02-20 23:45:10 +0530

    adding search suggestion

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ytLogo from "../assets/yt-logo.webp";
 import { FiMenu } from "react-icons/fi";
 import { IoIosSearch } from 'react-icons/io';
@@ -9,22 +9,53 @@ import { toggleSidebar } from '../store/headerSlice';
 
 const Header = () => {
 
+    const [searchQuery, setSearchSQuery] = useState("");
+    const [suggestions, setSuggessions] = useState([]);
+    const [showSuggessions, setShowSuggessions] = useState(false);
+
     const dispatch = useDispatch();
+
     const handleSidebar = () => {
         dispatch(toggleSidebar());
     }
 
+    useEffect(() => {
+        const timer = setTimeout(() => getSuggessions(), 200);
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [searchQuery])
+
+    const getSuggessions = async () => {
+        console.log(searchQuery);
+        const data = await fetch("http://suggestqueries.google.com/complete/search?client=youtube&ds=yt&client=firefox&q=" + searchQuery);
+        const json = await data.json();
+
+        console.log(json);
+        setSuggessions(json[1]);
+    }
     return (
         <header className='w-full h-[10vh] flex items-center justify-between gap-2 px-5 pr-10'>
             <div className='w-3/12 flex gap-4'>
-                <FiMenu className='text-xl cursor-pointer' 
-                    onClick={() =>handleSidebar()}
+                <FiMenu className='text-xl cursor-pointer'
+                    onClick={() => handleSidebar()}
                 />
-                <img alt="YouTube" src={ytLogo} className='w-28 object-cover cursor-pointer'/>
+                <img alt="YouTube" src={ytLogo} className='w-28 object-cover cursor-pointer' />
             </div>
-            <div className='w-6/12 flex items-center'>
-                <input type='text' placeholder='Search' className='w-8/12 px-4 py-2  border rounded-l-full outline-none ' />
-                <button className='px-4 py-[11px] text-lg border rounded-r-full outline-none bg-gray-50'><IoIosSearch /></button>
+            <div className='w-6/12 relative'>
+                <div className='w-full flex items-center'>
+                    <input type='text' placeholder='Search' className='w-8/12 px-4 py-2  border rounded-l-full outline-none '
+                        onChange={(e) => setSearchSQuery(e.target.value)}
+                        onFocus={()=> setShowSuggessions(true)}
+                        onBlur={()=> setShowSuggessions(false)}
+                    />
+                    <button className='px-4 py-[11px] text-lg border rounded-r-full outline-none bg-gray-50'><IoIosSearch /></button>
+                </div>
+                {showSuggessions && suggestions.length != 0 && <div className='w-8/12 absolute top-14 z-10 bg-white border border-gray-200 rounded-md'>
+                    <ul className=''>
+                        {suggestions.map(sug => <li key={sug} className='flex items-center gap-2 px-4 py-1 hover:bg-gray-100 cursor-default'><IoIosSearch className='text-lg' /> {sug}</li>)}
+                    </ul>
+                </div>}
             </div>
             <div className='w-1/12 flex justify-end gap-4 text-xl' >
                 <PiDotsThreeVertical />
